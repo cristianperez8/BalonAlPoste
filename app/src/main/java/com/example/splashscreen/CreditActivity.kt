47a7fda350943427323775097994f75f236a91a1
cristianperez8 <cpergar696@g.educaand.es>
2025-02-05 10:16:26 +0100

    Splash, MainActivity, CreditActivity y Consultas realizadas

---
+++ app/src/main/java/com/example/splashscreen/CreditActivity.kt
@@ -0,0 +1,41 @@
+package com.example.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class CreditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        setContentView(R.layout.credit_activity)
+
+        val username = intent.getStringExtra("USERNAME") ?: "Usuario"
+        val version = "1"
+        val nombreApp = "Balón al Poste"
+
+        val creditText: TextView = findViewById(R.id.credit_text)
+        creditText.text = "$username, estás usando la versión $version de $nombreApp."
+
+        val contactButton: Button = findViewById(R.id.boton_contactar)
+        contactButton.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "message/rfc822"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("alpostebalon@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Consulta de la app $nombreApp")
+                putExtra(Intent.EXTRA_TEXT, "¡Comentanos tus inquietudes!")
+            }
+            if (emailIntent.resolveActivity(packageManager) != null) {
+                startActivity(emailIntent)
+            } else {
+                Toast.makeText(this, "No hay aplicaciones de correo disponibles.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.example.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        setTheme(R.style.AppTheme)
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navigateButton: Button = findViewById(R.id.navigate_button)
+        val usernameInput: EditText = findViewById(R.id.username_input)
+
+        navigateButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val intent = Intent(this@MainActivity, CreditActivity::class.java).apply {
+                putExtra("USERNAME", username)
+            }
+            startActivity(intent)
         }
     }
 }
